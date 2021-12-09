e5da4367447eeb722bc26df165adb88e2c3b8255
Luis Santos <luis.santos@lab900.com>
2021-12-09 12:53:49 +0100

    fix: stop folder creation for ct and rt structs

--- preprocess/01_reorganize_folders.py
+++ preprocess/01_reorganize_folders.py
@@ -6,8 +6,9 @@ Created on Sat Sep  5 20:34:46 2020
 @author: elena
 """
 # import libraries
-import os 
+import os
 import shutil
+
 import pydicom
 from pydicom.dataset import Dataset
 
@@ -58,39 +59,34 @@ for patient in os.listdir(src):
                 modality = dcm_header.Modality
                 SeriesInstanceUID = dcm_header.SeriesInstanceUID
                 station_name = dcm_header.StationName
-                n_cbct_folder = os.path.join(cbctfolder,SeriesInstanceUID)
+            
+                source = os.path.join(srcfolder,f)
 
-                old_name = os.path.join(srcfolder,f)
-
-                if not os.path.exists(n_cbct_folder):
-                    os.mkdir(n_cbct_folder)
-                
                 if modality == 'CT':
-
-
                     if manufacturer == 'Varian Medical Systems' and station_name == "HALCYON_SPO":
+                        n_cbct_folder = os.path.join(cbctfolder,SeriesInstanceUID)
+                        if not os.path.exists(n_cbct_folder):
+                            os.mkdir(n_cbct_folder)
                         destination = n_cbct_folder
-                        source = os.path.join(srcfolder,f)
                         shutil.move(source, destination)
 
                     elif manufacturer == 'TOSHIBA':
                         destination = ctfolder
-                        source = os.path.join(srcfolder,f)
                         shutil.move(source, destination)
 
                 elif modality == 'RTSTRUCT':
                     new_name_rtstruct = os.path.join(srcfolder,"RTSTRUCT_"+f)
-                    os.rename(old_name,new_name_rtstruct)
+                    os.rename(source,new_name_rtstruct)
                     print("RTStruct is in the DB")
                 
                 elif modality == 'RTPLAN':
                     new_name_plan = os.path.join(srcfolder,"RTPLAN_"+f)
-                    os.rename(old_name,new_name_plan)
+                    os.rename(source,new_name_plan)
                     print("Plan is in the DB")
 
                 elif modality == 'RTDOSE':
                     new_name_dose = os.path.join(srcfolder,"RTDOSE_"+f)
-                    os.rename(old_name,new_name_dose)
+                    os.rename(source,new_name_dose)
                     print("Clincal doose is in the DB")
                 
                 else:
