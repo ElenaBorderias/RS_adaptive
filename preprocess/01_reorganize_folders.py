0dcc5a71caea46a29457ffd654b357a4e291a2cd
Luis Santos <luis.santos@lab900.com>
2021-12-09 18:01:42 +0100

    feat: preprocess folders is more efficient

--- preprocess/01_reorganize_folders.py
+++ preprocess/01_reorganize_folders.py
@@ -13,86 +13,92 @@ import pydicom
 from pydicom.dataset import Dataset
 
 # data path
-src = r"X:\Elena_test"
+src = os.path.abspath(r"/Users/comas/develop/elena/dicom_samples")
 # parse data folder
 roi_names = []
 
-"""
-##Margeries code
-for i in sorted(os.listdir(src)):
-    print(' ')
-    print(i)
-    if os.path.isdir(os.path.join(src,i)):
-        for j in os.listdir(os.path.join(src,i)):
-            print(j)
-            if '.dcm' in j: # verify that it is a DICOM image
-                dcm = pydicom.read_file(os.path.join(src,i,j))
-                if dcm.Modality == 'RTSTRUCT':
-                    for dcm_struct in dcm.StructureSetROISequence: 
-                        roi_names.append(dcm_struct.ROIName)
-                        print(dcm_struct.ROIName)
-"""
+
+def createFolder(folder, force=False):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    elif(not force):
+        raise Exception('folder: ' + folder + ' already exists.')
+
+
+def storePreprocess(patient_folder, dcm_file):
+    with open(os.path.join(patient_folder, 'reorganise_folders.txt'), 'a+') as f:
+        f.write(dcm_file)
+        f.write('\n')
+
+
+def getPreviousRun(patient_folder):
+    processed_files = []
+    with open(os.path.join(patient_folder, 'reorganise_folders.txt'), 'r') as f:
+        processed_files = [line.rstrip('\n') for line in f]
+    return processed_files
+
 
 # loop over patients
 for patient in os.listdir(src):
-    # get patient file
-    srcfolder = os.path.join(src,patient)
-    if (os.path.exists(os.path.join(srcfolder, 'CT'))):
-        continue;
-    
-    if not patient[0] == '.':
-        ctfolder = os.path.join(srcfolder,'CT')
-        cbctfolder = os.path.join(srcfolder,'CBCT')
-        if not os.path.exists(ctfolder):
-            os.mkdir(ctfolder)
-        if not os.path.exists(cbctfolder):
-            os.mkdir(cbctfolder)
+    patient_folder = os.path.join(src, patient)
+
+    if (os.path.isdir(patient_folder) and patient == 'ANON1'):
+        processed_files = getPreviousRun(patient_folder)
+        ct_folder = os.path.join(patient_folder, 'CT')
+        cbct_folder = os.path.join(patient_folder, 'CBCT')
+
+        createFolder(ct_folder, force=True)
+        createFolder(cbct_folder, force=True)
         # get useful info
         ct_uids = []
         ct_zcoord = []
         ct_sliceLoc = []
         ct_InstanceNbr = []
         # loop over files in patient
-        for f in os.listdir(srcfolder):
-            if '.dcm' in f:
-
-                dcm_header = pydicom.read_file(os.path.join(srcfolder,f),force = True)
+        for f in os.listdir(patient_folder):
+            dicom_file_path = os.path.join(patient_folder, f)
+            if os.path.isfile(dicom_file_path) and '.dcm' in f and f not in processed_files:
+                dcm_header = pydicom.read_file(dicom_file_path, force=True)
                 manufacturer = dcm_header.Manufacturer
                 modality = dcm_header.Modality
                 SeriesInstanceUID = dcm_header.SeriesInstanceUID
                 station_name = dcm_header.StationName
-            
-                source = os.path.join(srcfolder,f)
-
                 if modality == 'CT':
                     if manufacturer == 'Varian Medical Systems' and station_name == "HALCYON_SPO":
-                        n_cbct_folder = os.path.join(cbctfolder,SeriesInstanceUID)
-                        if not os.path.exists(n_cbct_folder):
-                            os.mkdir(n_cbct_folder)
+                        n_cbct_folder = os.path.join(
+                            cbct_folder, SeriesInstanceUID)
+                        createFolder(n_cbct_folder, force=True)
                         destination = n_cbct_folder
-                        shutil.move(source, destination)
+                        shutil.move(dicom_file_path, destination)
+                        storePreprocess(patient_folder, f)
 
                     elif manufacturer == 'TOSHIBA':
-                        destination = ctfolder
-                        shutil.move(source, destination)
+                        destination = ct_folder
+                        shutil.move(dicom_file_path, destination)
+                        storePreprocess(patient_folder, f)
 
                 elif modality == 'RTSTRUCT':
-                    new_name_rtstruct = os.path.join(srcfolder,"RTSTRUCT_"+f)
-                    os.rename(source,new_name_rtstruct)
+                    new_name_rtstruct = os.path.join(
+                        patient_folder, "RTSTRUCT_"+f)
+                    os.rename(dicom_file_path, new_name_rtstruct)
                     print("RTStruct is in the DB")
-                
+                    storePreprocess(patient_folder, "RTSTRUCT_"+f)
+
                 elif modality == 'RTPLAN':
-                    new_name_plan = os.path.join(srcfolder,"RTPLAN_"+f)
-                    os.rename(source,new_name_plan)
+                    new_name_plan = os.path.join(
+                        patient_folder, "RTPLAN_"+f)
+                    os.rename(dicom_file_path, new_name_plan)
                     print("Plan is in the DB")
+                    storePreprocess(patient_folder, "RTPLAN_"+f)
 
                 elif modality == 'RTDOSE':
-                    new_name_dose = os.path.join(srcfolder,"RTDOSE_"+f)
-                    os.rename(source,new_name_dose)
+                    new_name_dose = os.path.join(
+                        patient_folder, "RTDOSE_"+f)
+                    os.rename(dicom_file_path, new_name_dose)
                     print("Clincal doose is in the DB")
-                
+                    storePreprocess(patient_folder, "RTDOSE_"+f)
+
                 else:
                     print(f)
-                    print("This dicom is has not a valid modality in the dicom header")
-
-                
+                    print(
+                        "This dicom is has not a valid modality in the dicom header")
